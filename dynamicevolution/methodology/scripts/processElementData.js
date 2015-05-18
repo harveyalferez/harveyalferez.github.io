de322d734b13ede324e26a51aa5e304f1377cd5c
Harvey Alférez <harveyalferez@gmail.com>
2015-05-18 13:52:42 -0500

    New Version

---
+++ dynamicevolution/methodology/scripts/processElementData.js
@@ -0,0 +1,39 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_JhyO4YPEEeGjT6xUMMxnjAActivity"]="./../../dynamic_evolution/deliveryprocesses/resources/dynamic_evolution_2680987C_2680987c_Activity.jpeg"
+contentPage.processPage.elementUrls["_zMC-JIPSEeGjT6xUMMxnjA"] = ["Create the Weaving Model","dynamic_evolution/tasks/Create_Weaving_Model_BCA3077C.html"];
+contentPage.processPage.elementUrls["_zMEMQIPSEeGjT6xUMMxnjA"] = ["Analyst","dynamic_evolution/roles/Analyst_652663D.html"];
+contentPage.processPage.elementUrls["_zMC-I4PSEeGjT6xUMMxnjA"] = ["Create the Variability Model","dynamic_evolution/tasks/Create_Variability_Model_A11C55D5.html"];
+contentPage.processPage.elementUrls["_zMC-IoPSEeGjT6xUMMxnjA"] = ["Create the Context Model","dynamic_evolution/tasks/Create_Context_Model_6978FF86.html"];
+contentPage.processPage.elementUrls["_zMEzU4PSEeGjT6xUMMxnjA"] = ["Context Model","dynamic_evolution/workproducts/Context_Model_9E6A421D.html"];
+contentPage.processPage.elementUrls["_i_Lt9IPSEeGjT6xUMMxnjA"] = ["Variability Model","dynamic_evolution/workproducts/Variability_Model_EFFA22FD.html"];
+contentPage.processPage.elementUrls["_zMEzVYPSEeGjT6xUMMxnjA"] = ["Context Conditions","dynamic_evolution/workproducts/Context_Conditions_D6E74C9E.html"];
+contentPage.processPage.elementUrls["_zMEMQoPSEeGjT6xUMMxnjA"] = ["Requirements Model","dynamic_evolution/workproducts/Requirements_Model_9B7D02AF.html"];
+contentPage.processPage.elementUrls["_i9uVYIPSEeGjT6xUMMxnjA"] = ["Analyst","dynamic_evolution/roles/Analyst_652663D.html"];
+contentPage.processPage.elementUrls["_JhyO4YPEEeGjT6xUMMxnjA"] = ["Dynamic Evolution","dynamic_evolution/deliveryprocesses/dynamic_evolution_2680987C.html"];
+contentPage.processPage.elementUrls["_i_LG44PSEeGjT6xUMMxnjA"] = ["Context Conditions","dynamic_evolution/workproducts/Context_Conditions_D6E74C9E.html"];
+contentPage.processPage.elementUrls["_zMC-IIPSEeGjT6xUMMxnjA"] = ["Create the Architecture Model","dynamic_evolution/tasks/Create_Architecture_Model_F6106AD9.html"];
+contentPage.processPage.elementUrls["_zMEzV4PSEeGjT6xUMMxnjA"] = ["Variability Model","dynamic_evolution/workproducts/Variability_Model_EFFA22FD.html"];
+contentPage.processPage.elementUrls["_zMEzWYPSEeGjT6xUMMxnjA"] = ["Weaving Model","dynamic_evolution/workproducts/Weaving_Model_7291EBE3.html"];
+contentPage.processPage.elementUrls["_zMEzUYPSEeGjT6xUMMxnjA"] = ["Architecture Model","dynamic_evolution/workproducts/Architecture_Model_B53DDCB5.html"];
+contentPage.processPage.elementUrls["_i_Lt8YPSEeGjT6xUMMxnjA"] = ["Context Model","dynamic_evolution/workproducts/Context_Model_9E6A421D.html"];
+contentPage.processPage.elementUrls["_i9tuUIPSEeGjT6xUMMxnjA"] = ["Create the Requirements Model","dynamic_evolution/tasks/Create_Requirements_Model_768CAA15.html"];
+contentPage.processPage.elementUrls["_e2WA0KfjEeGiUZa-TbXCrQ"] = ["Create the Tactic Model","dynamic_evolution/tasks/Create_Tactic_Model_C08039F.html"];
+contentPage.processPage.elementUrls["_i_LG4IPSEeGjT6xUMMxnjA"] = ["Architecture Model","dynamic_evolution/workproducts/Architecture_Model_B53DDCB5.html"];
+contentPage.processPage.elementUrls["_e_OA0IPEEeGjT6xUMMxnjA"] = ["Design","dynamic_evolution/deliveryprocesses/Design_8876B908.html"];
+contentPage.processPage.elementUrls["_i_MVAYPSEeGjT6xUMMxnjA"] = ["Weaving Model","dynamic_evolution/workproducts/Weaving_Model_7291EBE3.html"];
+contentPage.processPage.elementUrls["_i9zN4IPSEeGjT6xUMMxnjA"] = ["Requirements Model","dynamic_evolution/workproducts/Requirements_Model_9B7D02AF.html"];
+contentPage.processPage.elementUrls["_zMC-IYPSEeGjT6xUMMxnjA"] = ["Create the Context Conditions","dynamic_evolution/tasks/Create_Context_Conditions_80FCC7E.html"];
+contentPage.processPage.elementUrls["_beDAIYPEEeGjT6xUMMxnjA"] = ["Requirements","dynamic_evolution/deliveryprocesses/Requirements_89640933.html"];
+contentPage.processPage.elementUrls["_4eHFMafjEeGiUZa-TbXCrQ"] = ["Tactic Model","dynamic_evolution/workproducts/Tactic_Model_AE84ADB5.html"];
